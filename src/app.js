a4c6b727eabe48dfde97d0085e5dafe00f24bd44
ductruonghoc <92839932+ductruonghoc@users.noreply.github.com>
2024-06-18 11:40:59 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = 3001;
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/assets')));
 
 app.get('/', (req, res) => {
-    res.render('home')
-});
+    res.send("Fuck!");
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
