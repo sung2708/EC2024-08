ad823fd06aa0dc4d76f4766335057d3a06262f68
ductruonghoc <92839932+ductruonghoc@users.noreply.github.com>
2024-07-25 01:09:38 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,33 @@
 const express = require('express');
-const app = express();
+const mysql = require('mysql')
 const path = require('path');
+const app = express()
 const port = 3001;
+
+const { promisify } = require('util');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.MYSQLPASSWORD,
+    database: 'EC2024'
+});
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/assets')));
 
-app.get('/', (req, res) => {
-    res.send("Fuck!");
-})
+const query = promisify(pool.query).bind(pool);
+
+app.get('/item', async (req, res) => {
+    try {
+        const [rows] = await query('SELECT * FROM ITEM');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error retrieving users');
+    }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
