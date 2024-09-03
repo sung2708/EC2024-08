56482cd132d097f28a7e9c1fd7fab923add21154
sung2708 <aki18082002@gmail.com>
2024-09-03 08:11:16 +0700

    Resolve merge conflict

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/order.controller');
+<<<<<<< main
+const cors = require('cors');
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+}
+
+router.use(cors(corsOptions));
+=======
+>>>>>>> Order
 
 // Route để lấy danh sách hóa đơn chưa hoàn thành
 router.post('/undone', orderController.getUndoneOrders);
